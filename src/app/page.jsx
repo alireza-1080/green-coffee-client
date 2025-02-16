ae4e4f16d3056c04e9d3e663ef8b506c1d250083
Alireza Aberoumand <maya7192@yahoo.com>
2025-02-15 16:30:05 -0800

    refactor: clean up Home component and restore header and products display

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,87 +1,14 @@
-// import React from 'react'
-// import HomePageHeader from '../components/templates/Home/HomePageHeader.jsx'
-// import LatestProducts from '../components/templates/Home/LatestProducts.jsx'
+import React from 'react'
+import HomePageHeader from '../components/templates/Home/HomePageHeader.jsx'
+import LatestProducts from '../components/templates/Home/LatestProducts.jsx'
 
-// const Home = () => {
-//   return (
-//     <div className='mb-96'>
-//       <HomePageHeader />
-//       <LatestProducts />
-//     </div>
-//   )
-// }
-
-// export default Home
-'use client';
-import React from 'react';
-
-const handleTestServerConnection = async () => {
-  const response = await fetch('/api', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-
-  const data = await response.json();
-
-  if (response.ok) {
-    console.log(data);
-  } else {
-    console.error(data);
-  }
-}
-
-const handleSetCookie = async () => {
-  const response = await fetch('/api/set-cookie', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    credentials: 'include',
-  });
-
-  const data = await response.json();
-
-  if (response.ok) {
-    console.log(data);
-  } else {
-    console.error(data);
-  }
-}
-
-const handleDeleteCookie = async () => {
-  const response = await fetch('/api/delete-cookie', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    credentials: 'include',
-  });
-
-  const data = await response.json();
-
-  if (response.ok) {
-    console.log(data);
-  } else {
-    console.error(data);
-  }
-}
-
-const HomePage = () => {
+const Home = () => {
   return (
-    <div className="w-full h-screen bg-gradient-to-r from-lime-500 to-sky-500 flex flex-col gap-8 justify-center items-center">
-      <button onClick={handleTestServerConnection} className="px-6 py-3 bg-gradient-to-r from-green-400 to-blue-500 text-white font-semibold rounded-lg shadow-md hover:from-green-500 hover:to-blue-600 active:from-green-600 active:to-blue-700 transition duration-300 ease-in-out">
-        Test Server Connection
-      </button>
-      <button onClick={handleSetCookie} className="px-6 py-3 bg-gradient-to-r from-green-400 to-blue-500 text-white font-semibold rounded-lg shadow-md hover:from-green-500 hover:to-blue-600 active:from-green-600 active:to-blue-700 transition duration-300 ease-in-out">
-        Set Cookie
-      </button>
-      <button onClick={handleDeleteCookie} className="px-6 py-3 bg-gradient-to-r from-red-400 to-yellow-500 text-white font-semibold rounded-lg shadow-md hover:from-red-500 hover:to-yellow-600 active:from-red-600 active:to-yellow-700 transition duration-300 ease-in-out">
-        Delete Cookie
-      </button>
+    <div className='mb-96'>
+      <HomePageHeader />
+      <LatestProducts />
     </div>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default Home

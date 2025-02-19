f5581d8cba2c94091cfd941fa0b3cd14d0e8bb8e
Alireza Aberoumand <maya7192@yahoo.com>
2025-02-19 00:47:40 -0800

    fix: update LatestArticlesSlider to use navigation instead of pagination; adjust spacing and layout for better responsiveness

--- src/components/modules/LatestArticles/LatestArticlesSlider.jsx
+++ src/components/modules/LatestArticles/LatestArticlesSlider.jsx
@@ -1,6 +1,5 @@
 'use client';
 import React from 'react';
-
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -9,54 +8,60 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 // import required modules
-import { Pagination } from 'swiper/modules';
+import { Navigation } from 'swiper/modules';
 
 const LatestArticlesSlider = ({ latestArticles }) => {
   return (
-    <div className="bg-white lg:container mx-auto overflow-hidden py-10 px-5">
+    <>
       <Swiper
+        spaceBetween={20}
         slidesPerView={1}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-          dynamicBullets: true, // Add dynamic bullets
-          dynamicMainBullets: 3, // Show max 3 main bullets
-        }}
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
         breakpoints={{
           640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
             slidesPerView: 2,
+            spaceBetween: 30,
           },
           1024: {
             slidesPerView: 3,
+            spaceBetween: 40,
           },
         }}
-        modules={[Pagination]}
-        className="mySwiper"
       >
         {latestArticles.map((article, index) => (
-            <SwiperSlide key={`${article.title}-${index}`} className="w-full h-fit overflow-hidden relative group max-w-80 xl:max-w-[393.33px] rounded-lg">
+          <SwiperSlide
+            key={`${article.title}-${index}`}
+            className="w-full h-fit overflow-hidden relative group max-w-full md:max-w-[393.33px] rounded-lg"
+          >
             <Link href={article.href} className="block">
               <Image
                 src={'/jpg/morning-coffee.jpg'}
                 width={1007}
                 height={1410}
                 alt={article.title}
+                className="w-full h-auto"
               ></Image>
               <div className="absolute top-5 left-5 flex flex-col items-center bg-green-dark text-white p-2 rounded-md">
                 <span>{parseInt(article.publishDate.day).toString()}</span>
                 <span>{article.publishDate.month}</span>
               </div>
-              <div className="absolute top-2/3 text-white bg-black/50 group-hover:bg-black/80 backdrop-blur-md m-2 p-2 rounded-md">
+              <div className="absolute bottom-0 left-0 right-0 text-white bg-black/50 group-hover:bg-black/80 backdrop-blur-md m-2 p-2 rounded-md">
                 <h2>{article.title}</h2>
               </div>
             </Link>
           </SwiperSlide>
         ))}
       </Swiper>
-    </div>
+    </>
   );
 };
 
--- src/components/modules/LatestArticles/LatestArticlesSliderSection.jsx
+++ src/components/modules/LatestArticles/LatestArticlesSliderSection.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import LatestProductsTopSection from './LatestArticlesTopSection';
 import LatestArticlesSlider from './LatestArticlesSlider';
 
-
 const LatestArticlesSliderSection = ({ latestArticles }) => {
   return (
     <div className="bg-white lg:container mx-auto overflow-hidden py-10 px-5">
-        <LatestProductsTopSection />
-        <LatestArticlesSlider latestArticles={latestArticles} />
+      <LatestProductsTopSection />
+      <LatestArticlesSlider latestArticles={latestArticles} />
     </div>
   );
 };
 
 export default LatestArticlesSliderSection;
+
+
+
+
+

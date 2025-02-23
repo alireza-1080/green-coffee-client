18509bf3cbbdf18cced0e0f700a45e11404afaeb
Alireza Aberoumand <maya7192@yahoo.com>
2025-02-23 14:55:21 -0800

    fix: remove console log from FooterContent component

--- src/components/modules/Footer/FooterContent.jsx
+++ src/components/modules/Footer/FooterContent.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import FooterListGroup from '@/components/modules/Footer/FooterListGroup';
 
 const FooterContent = ({ footerItems }) => {
-  console.log(footerItems);
-
   return (
     <div className="w-full bg-green-dark text-white py-10 px-5 md:text-lg lg:container lg:mx-auto">
       <div className="lg:flex lg:justify-between lg:gap-10">
